8249f5b2e09d7ef6f8941b78357c6b1c32c2cae6
asandler <setcursorpos@gmail.com>
2017-04-04 20:34:36 +0300

    fix testing script

--- solver/test.sh
+++ solver/test.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
+
+# $4 - spdi file
+# $5 - timeout (in seconds)
+
 mkdir -p tests
 
 for i in $(seq $1); do
     for j in $(seq $2); do
-        ../generator/task_generator ../examples/random_70.spdi "$i" "$j" > "tests/test_($i)_($j)"
+        ../generator/task_generator "$4" "$i" "$j" > "tests/test_($i)_($j)"
         for k in $(seq $3); do
             echo "$k" >> "tests/time_($i)_($j)"
-            { time ./solver --spdi ../examples/random_70.spdi --task "tests/test_($i)_($j)" --threads $k ; } >> "tests/time_($i)_($j)" 2>> "tests/time_($i)_($j)"
+            { time timeout $5 ./solver --spdi "$4" --task "tests/test_($i)_($j)" --threads $k ; } >> "tests/time_($i)_($j)" 2>> "tests/time_($i)_($j)"
         done
     done
-one
+done
